bcced7c49cd21bb77f1df7ca23a3a6a8f46cfecb
Irieda Noboru <irieda@144lab.com>
2022-07-04 10:00:59 +0900

    fix issue

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,10 +3,7 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
   kit: {
     adapter: adapter({
-      // default options are shown
-      pages: "build",
-      assets: "build",
-      fallback: null,
+      fallback: "index.html",
     }),
   },
 };
--- main.tmpl.go
+++ main.tmpl.go
@@ -10,7 +10,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
-//go:embed frontend/build
+//go:embed frontend/build/*
 var assets embed.FS
 
 func main() {
