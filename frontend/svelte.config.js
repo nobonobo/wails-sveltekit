ed0023ea05000f389b3eed149663e7d5cbfbc0c7
NoboNobo <irieda@gmail.com>
2023-05-23 11:22:49 +0900

    fix templates

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,14 +3,15 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $routes: path.resolve("./src/routes"),
-        },
-      },
-    },
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      strict: true,
+    }),
   },
 };
 
