b8206535b8f19c3d9ab514bc6b43a19063306921
Irieda Noboru <irieda@144lab.com>
2023-10-15 02:23:51 +0900

    support runtime & hot reload

--- frontend/src/routes/index.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>Welcome</title>
-</svelte:head>
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
